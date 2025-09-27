8a69ae0d3d7f8bc0b665104b1997eb4a535eab03
Eduardo Guzman <eduguzgar@gmail.com>
2025-09-27 21:35:47 +0200

    Protection against negative numbers

--- src/fibonacci/main.c
+++ src/fibonacci/main.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <limits.h>
+#include <stdint.h>
 #include "fibonacci.h"
 
-int main() {
-    size_t n;
-    long long *arr;
+int main(void) {
+    long tmp;
+    size_t n = 0;
+    long long *arr = NULL;
 
     printf("How many numbers do you want (N)?\n");
-    if (scanf("%zu", &n) != 1) {
+    fflush(stdout);
+
+    if (scanf("%ld", &tmp) != 1) {
         fprintf(stderr, "Error: invalid input\n");
-        return 1;
+        return EXIT_FAILURE;
+    }
+    if (tmp < 0) {
+        fprintf(stderr, "Error: negative numbers are not allowed\n");
+        return EXIT_FAILURE;
     }
 
-    arr = malloc(n * sizeof *arr);
+    n = (size_t) tmp;
 
-    if (!arr) {
-        fprintf(stderr, "Error: malloc array\n");
-        return 1;
+    if (n > 0 && n > SIZE_MAX / sizeof *arr) {
+        fprintf(stderr, "Error: requested size is too large\n");
+        return EXIT_FAILURE;
+    }
+
+    if (n > 0) {
+        arr = malloc(n * sizeof *arr);
+        if (!arr) {
+            fprintf(stderr, "Error: malloc failed\n");
+            return EXIT_FAILURE;
+        }
     }
 
     fibonacci(n, arr);
     print_fibonacci(n, arr);
 
     free(arr);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
